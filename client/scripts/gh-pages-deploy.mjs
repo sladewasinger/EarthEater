36c12db583ac33858076a81b8c938a411fde6f35
Austin Wasinger <austinwasinger@gmail.com>
2023-04-14 10:55:54 -0600

    How did I paste that wrong??

--- client/scripts/gh-pages-deploy.mjs
+++ client/scripts/gh-pages-deploy.mjs
@@ -11,7 +11,7 @@ import * as fs from 'fs';
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
-        await execa("git", ["push", "--force", "origin", "HEAD:gh-pages", "--force", "--set-upstream", "--receive-pack=git", `--repo=https://${process.env.GITHUB_TOKEN}@github.com/sladewasinger/
+        await execa("git", ["push", "--force", "origin", "HEAD:gh-pages", "--force", "--set-upstream", "--receive-pack=git", `--repo=https://${process.env.GITHUB_TOKEN}@github.com/sladewasinger/EarthEater.git`]);
         await execa("rm", ["-r", folderName]);
         await execa("git", ["checkout", "-f", "master"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
